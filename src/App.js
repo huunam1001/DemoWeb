bf1f9cb6da2f18dd7f8465a0a3a0c4d681d759d2
nam.nh <huunam101@gmail.com>
2023-02-27 17:28:54 +0700

    conitnued add resource

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Xin chào anh em F8!</h1>
+
+      <h2>Yêu anh em nhiều :*</h2>
+
+      <h3>Anh em có yêu lại không?</h3>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-function App() {
-    return (
-        <div>
-            <h1>Xin chào anh em F8!</h1>
-            <h2>Yêu anh em nhiều :*</h2>
-            <h3>Anh em có yêu lại không?</h3>
-        </div>
-    )
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 
-ReactDOM.render(<App />, document.getElementById('root'))
+  document.getElementById('root')
+)
