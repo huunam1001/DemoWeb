eef8f04428ac6dc15dc721f4e154a4a23f69f69b
nam.nh <huunam101@gmail.com>
2023-03-02 17:31:04 +0700

    contniued login layout

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import { Form, Input } from 'antd'
+import { Form, Input, Button, Typography } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import './style.scss'
 
 const Login = (props) => {
   return (
     <div className='loginBg'>
       <Form className='loginForm'>
-        <Form.Item label='Email'>
-          <Input />
+        <Typography.Text>LogIn</Typography.Text>
+        <Form.Item label=''>
+          <Input prefix={<UserOutlined className='site-form-item-icon' />} placeholder='Username' />
         </Form.Item>
-        <Form.Item label='Password'>
-          <Input />
+        <Form.Item label=''>
+          <Input prefix={<LockOutlined className='site-form-item-icon' />} type='password' placeholder='Password' />
+        </Form.Item>
+        <Form.Item shouldUpdate>
+          {() => (
+            <Button
+              type='primary'
+              htmlType='submit'
+              // disabled={
+              //   !form.isFieldsTouched(true) ||
+              //   !!form.getFieldsError().filter(({ errors }) => errors.length).length
+              // }
+            >
+              Log in
+            </Button>
+          )}
         </Form.Item>
       </Form>
     </div>
