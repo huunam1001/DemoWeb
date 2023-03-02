a1dc9c0c4b774ec43be656ff60416c75de776e62
nam.nh <huunam101@gmail.com>
2023-03-02 15:43:28 +0700

    add antd to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/home'
+import Login from './pages/Login/Login'
 
 const App = (props) => {
-  return (
-    <div>
-      <div>
-        <div>fasfadsf</div>
-        <div>fasfadsf</div>
-        <div>fasfadsf</div>
-      </div>
-
-      <BrowserRouter>
-        <Route path='/' element={Home} />
-      </BrowserRouter>
-    </div>
-  )
+  return <Login />
 }
 
 export default App
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,5 +1,20 @@
-const Login = () => {
-  return <div>Dashboard</div>
+import React from 'react'
+import { Form, Input } from 'antd'
+// import './style.scss'
+
+const Login = (props) => {
+  return (
+    <div className='loginBg'>
+      <Form>
+        <Form.Item label='Email'>
+          <Input />
+        </Form.Item>
+        <Form.Item label='Password'>
+          <Input />
+        </Form.Item>
+      </Form>
+    </div>
+  )
 }
 
 export default Login
