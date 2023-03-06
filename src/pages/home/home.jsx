949a6bc9afabefeba528d06f2439a6d564afc4d6
HuuNam <huunam101@gmail.com>
2023-03-06 22:15:59 +0700

    update home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -14,43 +14,45 @@ const { Header, Sider, Content } = Layout
 const Home = (props) => {
   return (
     <Layout>
-      <Sider>
-        <div className='logo' />
-
-        <Menu
-          theme='dark'
-          mode='inline'
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1'
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2'
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3'
-            }
-          ]}
-        />
-      </Sider>
-      <Layout className='site-layout'>
-        <Content
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280
-            // background: this.props.colorTheme.text7Color
-          }}
-        >
-          Content
-        </Content>
+      <Header className='header'></Header>
+      <Layout>
+        <Sider>
+          <div className='logo' />
+          <Menu
+            theme='light'
+            mode='inline'
+            defaultSelectedKeys={['1']}
+            items={[
+              {
+                key: '1',
+                icon: <UserOutlined />,
+                label: 'nav 1'
+              },
+              {
+                key: '2',
+                icon: <VideoCameraOutlined />,
+                label: 'nav 2'
+              },
+              {
+                key: '3',
+                icon: <UploadOutlined />,
+                label: 'nav 3'
+              }
+            ]}
+          />
+        </Sider>
+        <Layout className='site-layout'>
+          <Content
+            style={{
+              margin: '10px 10px',
+              padding: 24,
+              minHeight: 500,
+              background: 'red'
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
   )
