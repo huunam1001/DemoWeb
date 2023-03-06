bf40f77675055037433f4acc8b1975eb664f3d6c
HuuNam <huunam101@gmail.com>
2023-03-06 22:04:13 +0700

    change home menu screen

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
-import { Menu } from 'antd'
+import { Layout, Menu } from 'antd'
 import './style.scss'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined
+} from '@ant-design/icons'
+
+const { Header, Sider, Content } = Layout
 
 const Home = (props) => {
   return (
-    <div>
-      <Menu className='main_menu' items={[{ label: 'Dashboard' }, { label: 'Booking' }, { label: 'Shipment' }]}></Menu>
-      <div>Content</div>
-    </div>
+    <Layout>
+      <Sider>
+        <div className='logo' />
+
+        <Menu
+          theme='dark'
+          mode='inline'
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1'
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2'
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3'
+            }
+          ]}
+        />
+      </Sider>
+      <Layout className='site-layout'>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280
+            // background: this.props.colorTheme.text7Color
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   )
 }
 
