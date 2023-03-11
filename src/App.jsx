65f667143ee5e78a4bf7a8608470d869e3b99d42
nam.nh <huunam101@gmail.com>
2023-03-11 15:51:10 +0700

    fixed crash sytem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css'
 import Home from './pages/home/home'
-import Login from './pages/Login/Login'
 import AppRouter from './routes/app_router'
 
 const App = (props) => {
